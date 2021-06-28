870b68ff56a920d570b236b9373d3c0ceb75e0c4
Evelyn <evelyn.chavez92@yahoo.com>
2021-06-28 05:07:01 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove(main);
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+victory.innerHTML = "<h1>Evelyn is the champion</h1>";
+
